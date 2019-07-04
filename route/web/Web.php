7417434cae7afa92e6961b46874a9bd25346cc3b
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-07-04 20:29:17 -0300

    Configuração das rotas da web

--- route/web/Web.php
+++ route/web/Web.php
@@ -1,10 +1,36 @@
 <?php
 
+$conf = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$container = new \Slim\Container($conf);
 
+    
+$container['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) {
+        $statusCode = $exception->getCode() ? $exception->getCode() : 500;
+        
+        return $response->withStatus($statusCode)
+            ->withHeader('Content-Type', 'text/html')
+            ->write(\App\Controller\Exceptions\ExceptionWeb::setError(null, $exception->getCode()));
+    };
+};
+
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        $page = new \App\Controller\Page\Page();
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write($page->setTpl("error/404.html"));
+    };
+};
 
 $app = new \Slim\App($container);
+
 $app->add(new \Slim\HttpCache\Cache('public', 86400));
-$route = new \App\Controller\Route\AppController;
+
 
 $app->get('/', \App\Controller\Route\WebController::class.":index")->setName('home');
 
